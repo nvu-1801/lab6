3400587ffa3f14e6c6ff710a0e6f7caa32845a31
NguyenVu <phannguyenvupro@gmail.com>
2024-10-26 23:48:41 +0700

    Lab6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AddStudentPage from './pages/AddStudentPage';
 import { StudentProvider } from './Context/StudentContext';
 
 const App = () => (
@@ -9,7 +8,6 @@ const App = () => (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/add-student" element={<AddStudentPage />} />
       </Routes>
     </Router>
   </StudentProvider>
